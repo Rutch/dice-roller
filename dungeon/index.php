ebe8dc886ef04566725c9e44d2924c293026c6f4
Roger Nebot <roger.nebot@socialpoint.es>
2014-04-10 19:41:09 +0200

    asdf

--- dungeon/index.php
+++ dungeon/index.php
@@ -24,6 +24,7 @@ if (isset($_GET['logout']))
         <meta charset="UTF-8">
         <title>DAIgeons &amp; DAIgons</title>
         <link rel="stylesheet" type="text/css" href="styles/main.css">
+        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     </head>
 
     <body>
--- dungeon/menus/tirar-dados.php
+++ dungeon/menus/tirar-dados.php
@@ -1,6 +1,23 @@
-<?php
-	//TEMPLATE FOR CONTENT CREATION
-?>
+<script type="text/javascript">
+	//Define functions to be used!
+
+	function generateDiceRollerUrl()
+	{
+	<?php
+		echo "var name = '".$first."';";
+	?>
+		var dice = "&dice=";
+		dice += $("#diceRoller-dice-X").val()+"D"+$("#diceRoller-dice-Y").val();
+		var onlyMaster = "";
+		var sort = "";
+		var debug = "";
+		return "http://www.tupajar.com/diceRoller/diceRoller.php?player="+name+dice+onlyMaster+sort+debug;
+	}
+	function onClickGenerateDiceRollerUrl()
+	{
+		$("#diceRoller-txt-generate").val(generateDiceRollerUrl());
+	}
+</script>
 
 <div class="content-tirar-dados">
 	<h2>TIRAR DADOS</h2>
@@ -12,7 +29,7 @@
 				<div class="content-tirar-dados-form-element">
 					<fieldset>
 						<legend>Dado</legend>
-						<input style="width: 40px;" type="number" value="1"/> D <input style="width: 40px;" type="number" value="20"/>
+						<input id="diceRoller-dice-X" style="width: 40px;" type="number" value="1" min="1"/> D <input id="diceRoller-dice-Y" style="width: 40px;" type="number" value="20" min="1"/>
 					</fieldset>
 				</div>
 				<div class="content-tirar-dados-form-element">
@@ -40,9 +57,9 @@
 				<div class="content-tirar-dados-form-element" style="clear: left;">
 					<fieldset>
 						<legend>Tirar dados!</legend>
-						<button>Tirar dados</button>
-						<button>Generar URL</button>
-						<input style="width: 626px;" type="text" readonly="readonly"/>
+						<button type="button" >Tirar dados</button>
+						<button onClick="onClickGenerateDiceRollerUrl()" type="button" id="diceRoller-btn-generate">Generar URL</button>
+						<input id="diceRoller-txt-generate" style="width: 626px;" type="text" readonly="readonly"/>
 					</fieldset>
 				</div>
 			<div>
@@ -63,3 +80,8 @@
 		</fieldset>
 	</form>
 </div>
+
+<script type="text/javascript">
+	//Auto-selecting url on dice-roller
+	$("#diceRoller-txt-generate").click(function() { $(this).select() });
+</script>
